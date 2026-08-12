8409f4ab944ecf533a722236e04ec27484c057ed
Musaib Ahmed <musaib.ahmed@trioaillc.com>
2026-08-13 01:42:33 +0530

    NumPy learning examples

---
+++ main.py
@@ -0,0 +1,45 @@
+import numpy as np
+
+#numbers = np.array([10, 20, 30, 40, 50])
+
+# Accessing elements
+#first = numbers[0]
+#last = numbers[-1]
+
+# Changing an element
+#numbers[2] = 100
+
+# Creating an array of zeros
+#zeros = np.zeros(5)
+
+# Creating an array of ones
+#ones = np.ones(5)
+
+# Creating a sequence
+#sequence = np.arange(1, 11)
+
+#print(numbers)
+
+
+
+2.#########MULTIDIMENSIONAL ARRAYS#########
+
+#matrix = np.array([
+ #   [1, 2, 3],
+  #  [4, 5, 6],
+   # [7, 8, 9]
+#])
+
+# Access specific elements
+#element = matrix[1, 2]
+
+# Change an element
+#matrix[0, 0] = 100
+
+# Get a complete row
+#row = matrix[1]
+
+# Get a complete column
+#column = matrix[:, 1]
+
+#print(matrix)
---
+++ slicing.py
@@ -0,0 +1,23 @@
+import numpy as np
+
+#numbers = np.array([10, 20, 30, 40, 50, 60, 70])
+
+#first_three = numbers[:3]
+
+#middle = numbers[2:5]
+
+#every_second = numbers[::2]
+
+#reversed_array = numbers[::-1]
+
+# 2D slicing
+#matrix = np.array([
+ #   [1, 2, 3],
+  #  [4, 5, 6],
+   # [7, 8, 9]
+#])
+
+#top_two_rows = matrix[:2]
+#right_two_columns = matrix[:, 1:]
+
+#print(middle)
